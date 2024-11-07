bbd9914ce3b3af7940f59d14545edb54562d4b96
Brian Isaac <kostadinov.shalon@gmail.com>
2024-11-07 12:21:16 +0000

    Modifications for training image net

--- classification/CIFAR/train_dream-ood_det_in100.py
+++ classification/CIFAR/train_dream-ood_det_in100.py
@@ -124,8 +124,8 @@ np.random.seed(args.seed)
 mean = [x / 255 for x in [125.3, 123.0, 113.9]]
 std = [x / 255 for x in [63.0, 62.1, 66.7]]
 
-traindir = os.path.join(args.id_root)
-valdir = os.path.join(args.id_root)
+traindir = os.path.join(args.id_root, 'train')
+valdir = os.path.join(args.id_root, 'val')
 normalize = trn.Normalize(mean=[0.485, 0.456, 0.406],
                           std=[0.229, 0.224, 0.225])
 
@@ -282,7 +282,7 @@ def train_permute():
 
     batch_iterator = iter(train_loader_out)
     for _, in_set in enumerate(train_loader_in):
-        print(_)
+
         try:
             out_set = next(batch_iterator)
         except StopIteration:
